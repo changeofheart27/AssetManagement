a643eebec182b3ddd1288d721c0aedba4e82155a
Hieu Duong <hieudhanu27@gmail.com>
2021-08-02 15:55:33 +0700

    add project structure files and folder for FE and BE

---
+++ Back-End/asset-management-web-service/src/main/java/com/nashtech/assetmanagementwebservice/AssetManagementWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nashtech.assetmanagementwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagementWebServiceApplication.class, args);
+	}
+
+}
---
+++ Back-End/asset-management-web-service/src/test/java/com/nashtech/assetmanagementwebservice/AssetManagementWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashtech.assetmanagementwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
