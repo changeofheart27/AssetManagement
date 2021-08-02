11bf2c86b19c8f905fb800394d421252dd23d395
nguyendinhinit <48084269+nguyendinhinit@users.noreply.github.com>
2021-08-02 23:18:47 +0700

    FE Edit Asset

--- asset-management-web-ui/src/pages/edit/Edit.js
+++ asset-management-web-ui/src/pages/edit/Edit.js
@@ -1,40 +1,87 @@
 import React from 'react';
-import {Container, Table} from "react-bootstrap";
+import {Container, Form, FormControl, InputGroup, Button, FormCheck, Row} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Edit = () => {
     return (
         <Container>
-            Edit
-            <Table striped bordered hover size="sm">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td colSpan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-                </tbody>
-            </Table>
+            <h1 className={"text-danger"}>Edit User</h1>
+            <Form>
+                <Row className={"mb-3"}>
+                    <p className={"w-25"}>Name</p>
+                    <FormControl
+                        placeholder="Username"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        className={"w-75"}
+                    />
+                </Row>
+                <Row className="mb-3">
+                    <p className={"col-3"}>Category</p>
+                    <Form.Select size="lg" className={"w-75"}>
+                        <option>Laptop</option>
+                        <option>Monitor</option>
+                        <option>Personal Computer</option>
+                    </Form.Select>
+                </Row>
+                <Row className="mb-3">
+                    <p className={"w-25"}>Specification</p>
+                    <FormControl
+                        placeholder="Username"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        className={"w-75"}
+                    />
+                </Row>
+                <Row className="mb-3">
+                    <p className={"w-25"} id="basic-addon1">Install Date</p>
+                    <FormControl
+                        type={"date"}
+                        placeholder="Last Name"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        className={"w-75"}
+                    />
+                </Row>
+                <Row className="mb-3">
+                    <p id="basic-addon1" className={"me-5 w-25"}>State</p>
+                    <div className={"container"}>
+                        <FormCheck
+                            inline
+                            type={"radio"}
+                            label={"Available"}
+                        >
+                        </FormCheck>
+                        <FormCheck
+                            inline
+                            type={"radio"}
+                            label={"Not available"}
+                        >
+                        </FormCheck>
+
+                        <FormCheck
+                            inline
+                            type={"radio"}
+                            label={"Waiting for recycling"}
+                        >
+                        </FormCheck>
+
+                        <FormCheck
+                            inline
+                            type={"radio"}
+                            label={"Recycled"}
+                        >
+                        </FormCheck>
+                    </div>
+                </Row>
+                <Button variant={"danger"} type={"submit"}>
+                    Save
+                </Button>
+                <Button variant={"danger"} type={"submit"} className={"ms-5"}>
+                    Cancel
+                </Button>
+            </Form>
         </Container>
     );
 };
