fd5a1dcb326c408aa1ed62a013582b95da7c6466
daoninhthai <thaimeo1131@gmail.com>
2021-08-05 15:10:28 +0700

    update

--- asset-management-web-service/src/main/java/com/nashtech/assetmanagementwebservice/repository/UserRepository.java
+++ asset-management-web-service/src/main/java/com/nashtech/assetmanagementwebservice/repository/UserRepository.java
@@ -10,6 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     public User findByUsername(String username);
-
+    
 
 }
