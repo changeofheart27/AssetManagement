0f465ea899029c00db57ffcc74867861f5577a41
namkattor123 <namkattor0612>
2021-08-14 10:49:04 +0700

    Login Page

--- asset-management-web-ui/src/layout/header/LoginFormPage.js
+++ asset-management-web-ui/src/layout/header/LoginFormPage.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { Formik } from 'formik';
 import { useState } from "react";
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as Yup from "yup";
+import {Button, Form, FormCheck, FormControl, Row} from "react-bootstrap";
 const LoginFormPage = ({setCurrentUser}) => {
   
     
     const initialValues = { username:'', password:''};
     const [loginSuccess, setLoginSuccess] = useState(false);
-    const validateValues = values => {
-      const errors = {};
-      if(!values.username){
-        errors.username = 'Required username';
-      }else if (values.username.length < 5) {
-        errors.username = "*Password must be greater than 5 characters long.";
-      }
-      if(!values.password){
-        errors.password = 'Required password';
-      }else if (values.password.length < 8) {
-        errors.password = "*Password must be 8 characters long.";
-      }
-    }
+    const ValidateSchema = Yup.object().shape({
+      username: Yup.string()
+          .max(10)
+          .required('Required')
+          .typeError('Username is required'),
+      password: Yup.string()
+          .max(15)
+          .required('Required')
+          .typeError('Password is required'),    
+  });
     const onSubmit = (values, { setSubmitting }) => {
       axios.post('',{
         username: values.username,
@@ -39,66 +39,75 @@ const LoginFormPage = ({setCurrentUser}) => {
       });
     }
     return (
-    <div >
-     <h1>Login !</h1>
-     <Formik
-       initialValues={initialValues}
-       validate={validateValues}
-       onSubmit={onSubmit}
-     >
-      {({
-         values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting,
-         
-       }) => (
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                           <label for="username">Username</label>
-                            <input
-                              
-                              type="username"
-                              name="username"
-                              className="form-control"
-                              placeholder = "username"
-                              onChange={handleChange}
-                              onBlur={handleBlur}
-                              value={values.username}
-                              required/>
-                            {errors.username && touched.username && errors.username}
-                        </div>  
-                        <div className="form-group">
-                           <label for="password">Password</label>
-                           <input
-                            type="password"
-                            name="password"
-                            className="form-control"
+    <div className={"container ps-5 d-block"} >
+     <Row>
+        <h1 className={"text-danger mb-5"}>Login</h1>
+      </Row>
+    <Row className={"mt-5"}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={ValidateSchema}
+        onSubmit={onSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          
+        }) => (
+            <Form onSubmit={handleSubmit}>
+              <Row className={"mb-3"}>
+                <p className={"w-25"}>Username</p>
+                                <FormControl
+                                    aria-label="Username"
+                                    aria-describedby="basic-addon1"
+                                    className={"w-75"}
+                                    name={"username"}
+                                    onChange={handleChange}
+                                    isValid={touched.username && !errors.username}
+                                    isInvalid={touched.username && errors.username}
+                                />
+                                {errors.username && touched.username ? (
+                                    <div className={"text-danger"} style={{paddingLeft: "25%"}}>{errors.username}</div>
+                                ) : null}
+                            </Row>
+                            <Row className={"mb-3"}>
+                                <p className={"w-25"}>Password</p>
+                                <FormControl
+                                    aria-label="Password"
+                                    aria-describedby="basic-addon1"
+                                    className={"w-75"}
+                                    name={"password"}
+                                    onChange={handleChange}
+                                    isValid={touched.password && !errors.password}
+                                    isInvalid={touched.password && errors.password}
+                                />
+                                {errors.password && touched.password ? (
+                                    <div className={"text-danger"} style={{paddingLeft: "25%"}}>{errors.password}</div>
+                                ) : null}
+                            </Row>
                             
-                            placeholder =" password"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.password}
-                            required/>
-                          {errors.password && touched.password && errors.password}
-                        </div>
-                        
-                          <div className="modal-footer">
-                            <button className = "edit-button"  style={{margin : 15, display: "block" }} type="submit" disabled={isSubmitting}>
-                              Submit
-                            </button>
-                          </div>
-                     </form>
-                
-            
-         
-        
-       )
-      } 
-     </Formik>
+                            <Button variant={"danger"} type={"submit"} className={"ms-5"} style={{float: 'right'}}>
+                                Cancel
+                            </Button>
+                          
+                            <Button variant={"danger"} type={"submit"} style={{float: 'right'}} on>
+                                Submit
+                            </Button>
+                            
+                      </Form>
+                  
+              
+          
+          
+        )
+        } 
+      </Formik>
+    </Row>
    </div>
     );
 }
